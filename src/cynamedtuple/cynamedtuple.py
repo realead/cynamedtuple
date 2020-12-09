7a33995a49f7f0b2157821aac0e5bf31e16d90f3
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2020-12-09 23:32:44 +0100

    adding __getitem__

--- src/cynamedtuple/cynamedtuple.py
+++ src/cynamedtuple/cynamedtuple.py
@@ -17,9 +17,18 @@ def _create_init_signature(names):
 
 
 def _create_init_body(names):
-    inits = [_TAB+_TAB+"self."+x+" = "+x for x in names]
+    inits = [_TAB*2+"self."+x+" = "+x for x in names]
     return inits
 
+
+def _create_getitem(names):
+    code = [_TAB + "def __getitem__(self, i):"]
+    n = len(names)
+    for i, name in enumerate(names):
+        line = "if i=={pos} or i=={neg}: return self.{name}".format(pos=i, neg=-n+i, name=name)
+        code.append(_TAB*2+line)
+    code.append(_TAB*2+"raise IndexError('tuple index out of range')")
+    return code
 
 def _ensure_as_pairs(name_types):
     if isinstance(name_types, dict):
@@ -34,6 +43,7 @@ def _create_cdef_class_code(classname, name_ctype_pairs):
     code_lines.extend(_create_members_definition(name_ctype_pairs))
     code_lines.append(_create_init_signature(names))
     code_lines.extend(_create_init_body(names))
+    code_lines.extend(_create_getitem(names))
     return "\n".join(code_lines)+"\n"
 
 
--- tests/unit_tests/test_cynamedtuple.py
+++ tests/unit_tests/test_cynamedtuple.py
@@ -27,6 +27,26 @@ class CynamedtupleTester(unittest.TestCase):
         self.assertEqual(a.a, 42)
         self.assertEqual(a.b, 21)
 
+    def test_typed_namedtuple_getitem_positive(self):
+        A = typed_namedtuple("A", (("a", "int"), ("b", "int"), ("c", "int")))
+        a = A(1,2,3)
+        self.assertEqual(a[0], 1)
+        self.assertEqual(a[1], 2)
+        self.assertEqual(a[2], 3)
+        with self.assertRaises(IndexError) as context:
+            a[3]
+        self.assertEqual("tuple index out of range", context.exception.args[0])
+
+    def test_typed_namedtuple_getitem_negative(self):
+        A = typed_namedtuple("A", (("a", "int"), ("b", "int"), ("c", "int")))
+        a = A(1,2,3)
+        self.assertEqual(a[-1], 3)
+        self.assertEqual(a[-2], 2)
+        self.assertEqual(a[-3], 1)
+        with self.assertRaises(IndexError) as context:
+            a[-4]
+        self.assertEqual("tuple index out of range", context.exception.args[0])
+
     def test_untyped_namedtuple_cycode(self):
         cycode = untyped_namedtuple_cycode("BBB", ("a", "b"))
         self.assertTrue("cdef class BBB:" in cycode, msg=cycode)
@@ -38,3 +58,23 @@ class CynamedtupleTester(unittest.TestCase):
         a = A(42, 21)
         self.assertEqual(a.a, 42)
         self.assertEqual(a.b, 21)
+
+    def test_untyped_namedtuple_getitem_positive(self):
+        A = untyped_namedtuple("A", ("a", "b", "c"))
+        a = A(1,2,3)
+        self.assertEqual(a[0], 1)
+        self.assertEqual(a[1], 2)
+        self.assertEqual(a[2], 3)
+        with self.assertRaises(IndexError) as context:
+            a[3]
+        self.assertEqual("tuple index out of range", context.exception.args[0])
+
+    def test_untyped_namedtuple_getitem_negative(self):
+        A = untyped_namedtuple("A", ("a", "b", "c"))
+        a = A(1,2,3)
+        self.assertEqual(a[-1], 3)
+        self.assertEqual(a[-2], 2)
+        self.assertEqual(a[-3], 1)
+        with self.assertRaises(IndexError) as context:
+            a[-4]
+        self.assertEqual("tuple index out of range", context.exception.args[0])
